a25cd32b3e5a190f09f8328316fd7d1efb279b19
Arteom Nesterov <arteomnesterov@MacBook-Air-Arteom.local>
2023-06-11 18:37:53 +0300

    first commit

---
+++ Money.cs
@@ -0,0 +1,181 @@
+﻿using System;
+
+namespace SelfEdu
+{
+    public class Money
+    {
+        public string? Rouble;
+        public string? R_Currency;
+        public string? Peny;
+        public string? P_Currency;
+
+        public Money(string money, string currency)
+        {
+            if (Convert.ToInt32(money) < 0)
+                Console.WriteLine("Не может быть отрицательным!");
+            else if(currency == "коп." && Convert.ToInt32(money) > 99)
+            {
+                if(Convert.ToInt32(money) % 100 == 0)
+                {
+                    Rouble = Convert.ToString(
+                        Convert.ToInt32(money) / 100);
+                    Peny = "0";
+                }
+                else if(Convert.ToInt32(money) % 100 != 0)
+                {
+                    Rouble = Convert.ToString(
+                        Convert.ToInt32(money) / 100);
+                    Peny = Convert.ToString(
+                        Convert.ToInt32(money) % 100);
+                }
+                R_Currency = "р.";
+                P_Currency = currency;
+            }
+            else if(currency == "р.")
+            {
+                Rouble = money;
+                R_Currency = currency;
+                Peny = "0";
+                P_Currency = "коп.";
+            }
+            else if(currency == "коп.")
+            {
+                Peny = money;
+                Rouble = "0";
+                R_Currency = "р.";
+                P_Currency = currency;
+            }
+            else
+            {
+                throw new ArgumentException("Не валидная сумма или валюта!");
+            }
+        }
+
+        public Money(string rub,
+            string r_curr,
+            string peny,
+            string p_curr):this(peny, p_curr)
+        {
+            if (r_curr == "коп." || p_curr == "р.")
+                Console.WriteLine("Рубли и копейки поменялись местами");
+            else if (Convert.ToInt32(rub) < 0)
+                Console.WriteLine("Не может быть отрицательным!");
+            else if(Convert.ToInt32(peny) > 99)
+            {
+                Rouble = Convert.ToString(
+                    Convert.ToInt32(Rouble) + Convert.ToInt32(rub)); 
+            }
+            else
+            {
+                Rouble = rub;
+                R_Currency = r_curr;
+                Peny = peny;
+                P_Currency = p_curr;
+            }
+            R_Currency = r_curr;
+            P_Currency = p_curr;
+        }
+        public static Money Sum(Money a, Money b)
+        {
+            var rubPart = Convert.ToString(
+                Convert.ToInt32(a.Rouble) + Convert.ToInt32(b.Rouble));
+            var penyPart = Convert.ToString(
+                Convert.ToInt32(a.Peny) + Convert.ToInt32(b.Peny));
+            return new Money(rubPart, "р.", penyPart, "коп.");
+        }
+
+        public static Money Difference(Money a, Money b)
+        {
+            if(Convert.ToInt32(a.Peny) == 0 && Convert.ToInt32(b.Peny) == 0)
+            {
+                var rubParts = Convert.ToString(
+                    Convert.ToInt32(a.Rouble) - Convert.ToInt32(b.Rouble));
+                return new Money(rubParts, "р.", "0", "коп.");
+            }
+            else if(Convert.ToInt32(a.Peny) == 0 || Convert.ToInt32(b.Peny) == 0)
+            {
+                if(Convert.ToInt32(a.Peny) == 0)
+                {
+                    var rubDif = Convert.ToInt32(a.Rouble) - Convert.ToInt32(b.Rouble);
+                    var dif = (rubDif * 100) - Convert.ToInt32(b.Peny);
+                    var rubParts = Convert.ToString(dif / 100);
+                    var penyParts = Convert.ToString(dif % 100);
+                    return new Money(rubParts, "р.", penyParts, "коп.");
+                }
+                else if(Convert.ToInt32(b.Peny) == 0)
+                {
+                    var rubDif = Convert.ToInt32(b.Rouble) - Convert.ToInt32(a.Rouble);
+                    var dif = (rubDif * 100) - Convert.ToInt32(a.Peny);
+                    var rubParts = Convert.ToString(dif / 100);
+                    var penyParts = Convert.ToString(dif % 100);
+                    return new Money(rubParts, "р.", penyParts, "коп.");
+                }
+            }
+            else if((Convert.ToInt32(a.Peny) < Convert.ToInt32(b.Peny))
+                || (Convert.ToInt32(b.Peny) < Convert.ToInt32(a.Peny)))
+            {
+                if(Convert.ToInt32(a.Rouble) != 0 && Convert.ToInt32(b.Rouble) == 0)
+                {
+                    var rub = a.Rouble + a.Peny;
+                    var res = Convert.ToInt32(rub);
+                    var dif = res - Convert.ToInt32(b.Peny);
+                    if(dif % 100 == 0)
+                    {
+                        var rubParts = Convert.ToString(
+                            dif / 100);
+                        return new Money(rubParts, "р.", "0", "коп.");
+                    }
+                    else
+                    {
+                        var rubParts = Convert.ToString(
+                            dif / 100);
+                        var penyParts = Convert.ToString(
+                            dif % 100);
+                        return new Money(rubParts, "р.", penyParts, "коп.");
+                    }
+                }
+                else if (Convert.ToInt32(b.Rouble) != 0 && Convert.ToInt32(a.Rouble) == 0)
+                {
+                    var rub = b.Rouble + b.Peny;
+                    var res = Convert.ToInt32(rub);
+                    var dif = res - Convert.ToInt32(a.Peny);
+                    if (dif % 100 == 0)
+                    {
+                        var rubParts = Convert.ToString(
+                            dif / 100);
+                        return new Money(rubParts, "р.", "0", "коп.");
+                    }
+                    else
+                    {
+                        var rubParts = Convert.ToString(
+                            dif / 100);
+                        var penyParts = Convert.ToString(
+                            dif % 100);
+                        return new Money(rubParts, "р.", penyParts, "коп.");
+                    }
+                }
+            }
+            var rubPart = Convert.ToString(
+                    Convert.ToInt32(a.Rouble) - Convert.ToInt32(b.Rouble));
+            var penyPart = Convert.ToString(
+                Convert.ToInt32(a.Peny) - Convert.ToInt32(b.Peny));
+            return new Money(rubPart, "р.", penyPart, "коп.");
+        }
+        public void PrintTransferedCount(double n)
+        {
+            double num = double.Parse(Rouble + "." + Peny);
+            var res = Math.Round(num / 100 * n + num, 2);
+            string[]? reverseConverter = Convert.ToString(res).Split(".");
+            var rub = reverseConverter[0];
+            var peny = reverseConverter[1];
+            Console.WriteLine($"{rub} р. {peny}  коп.");
+
+        }
+        public void Print()
+        {
+            if (Convert.ToInt32(Rouble) != 0 || Convert.ToInt32(Peny) != 0)
+                Console.WriteLine($"{Rouble} {R_Currency} {Peny} {P_Currency}");
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿// 1. Create class Money
+// 2. init fields Rouble,Rouble_Currency, Peny, Peny_Currency
+// 3. init constructor {a,rub or kop} or {a,b,c,d}
+// 4. Verify if peny > 99; curr_r == 'kop.'or  curr_r == 'p.' => PrintExcept; value < 0 => PrintExcept
+// 5. Create method static method Sum(inst a, inst b) -> new obj ClassInsts
+// 6. -||- (fixed name method to Difference)
+// 7. Create method PrintTransferedCount(double x): formula((sum / 100 * x) + sum)
+// 8. Create method Print() -> print sum to console
+
+// cases:
+// 1. input("-10", "p.") -> Console.WriteLine();
+// 2. input("10", "p.", "251", "kop.") -> 12 p. 51 kop.;
+// 3. Diff obj1 input("2"," p", "0") - Diff obj2("3", "p.", "90", "kop.") -> -1 p. 10 kop.;
+/*
+ void HelperValidator(inst a , inst b)
+{
+    (if a.Peny == 0 && b.Peny == 0)
+    {
+        var tmp = Convt.Toint32(a.Rouble) - Covn.Int32(b.Rouble);
+        Rouble = Convt.ToStr(tmp);
+    }
+    var tmp = Convrt.ToInt32(a.Roub) * 100 - Convr.Toint32(b.Peny);
+    Rouble = Convt.ToString(Convt.ToInt32(tmp / 100));
+    Peny = Convt.ToStrint(Convtr.ToInt32(tmp) % 100));
+}
+ */
+
+
+using System;
+
+
+class Program
+{
+    public static void Main()
+    {
+        var testObjA = new SelfEdu.Money("10", "р.", "15", "коп.");
+        var testObjB = new SelfEdu.Money("0", "руб", "75", "коп.");
+        var sum = SelfEdu.Money.Sum(testObjA, testObjB);
+        sum.Print();
+        var diff = SelfEdu.Money.Difference(testObjA, testObjB);
+        diff.Print();
+        sum.PrintTransferedCount(5);
+        diff.PrintTransferedCount(0.05);
+
+
+    }
+}
